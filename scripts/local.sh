a955ec6cf0910c80788097106a79fd937df5e82a
Jefferson Lisboa de Souza <lisboa.jeff@gmail.com>
2024-04-13 15:08:03 -0300

    Refactor project structure and update action files
    The setup.py file has been removed and its dependencies have been moved to requirements.txt, enhancing the setup process clarity. The workflow file name and its content have been modified to take input parameters dynamically during the workflow dispatch. A new 'env.sh' script has been created in the 'scripts' directory to load environment configurations. Also, dependabot.yml has been added to facilitate package updates, and a local.sh script has been introduced to automate local setup and run processes.

---
+++ scripts/local.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+COUNTRY=$1
+ENVIRONMENT=$2
+CONFIG_PATH="$COUNTRY/$ENVIRONMENT/config.env"
+VENV="/tmp/venv_bucket"
+SOURCE="src"
+
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed. Install Python 3 to continue."
+    exit 1
+fi
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <COUNTRY> <ENVIRONMENT>"
+    exit 1
+fi
+
+if [ ! -d "$VENV" ]; then
+    python3 -m venv "$VENV"
+    echo "Virtual environment '$VENV' created."
+else
+    echo "The virtual environment '$VENV' already exists."
+fi
+
+source "$VENV/bin/activate"
+
+if [ ! -f "$SOURCE/requirements.txt" ]; then
+    echo "File requirements.txt not found."
+    exit 1
+else
+    pip install -r "$SOURCE/requirements.txt"
+fi
+
+if [ ! -f "$CONFIG_PATH" ]; then
+    echo "Configuration file not found: $CONFIG_PATH"
+    exit 1
+fi
+
+set -a
+# shellcheck disable=SC1090
+source "$CONFIG_PATH"
+set +a
+
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+echo "AWS Access Key ID and/or AWS Secret Access Key have not been set as environment variables."   exit 1
+fi
+
+python3 "$SOURCE/main.py" "$COUNTRY" "$ENVIRONMENT"
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='sync_certificates',
-    version='0.1',
-    packages=find_packages(),
-    package_dir={'': 'src'},
-    install_requires=['boto3']
-)
